c833dcabe62319bc8cd14421b77d820e8d672d1b
alexsoftpnz <loshchinin_av@mail.ru>
2017-01-17 15:48:59 +0300

    fix

--- Delphi/Source/srcMain.pas
+++ Delphi/Source/srcMain.pas
@@ -4,11 +4,17 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, System.ImageList,
+  Vcl.ImgList;
 
 type
   TfrmMain = class(TForm)
+    lblUser: TLabel;
+    lblPass: TLabel;
+    edtUserName: TEdit;
+    edtUserPass: TEdit;
     Button1: TButton;
+    ImageList1: TImageList;
   private
     { Private declarations }
   public
